09016d0c09b013c33de375a591428cbaa6a59a00
Guilllaume <bande.guillaume@gmail.com>
2021-07-02 17:01:01 +0200

    Ajout du css

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,42 +8,75 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
-    /**
-     * @Route("/articlelist", name="articlelist")
-     */
-    public function listeArticle()
-    {
-        //retourne une reponse http valide
-        return new Response('article');
-    }
-    /**
-     * @Route("/article/{id}", name="articleshow")
-     */
-    public function articlesShow($id)
-    {
-        $articles = [
-            '1' => [
-                "title" => "Titre 1",
-                "content" => "blablabla",
-                "id" => 1
-            ],
-            '2' => [
-                "title" => "Titre 2",
-                "content" => "blablabla",
-                "id" => 2
-            ],
-            '3' => [
-                "title" => "Titre 3",
-                "content" => "blablabla",
-                "id" => 3
-            ]
 
-        ];
+    private $articles = [
+        1 => [
+            "title" => "La vaccination c'est trop géniale",
+            "content" => "bablablblalba",
+            "id" => 1
+        ],
+        2 => [
+            "title" => "La vaccination c'est pas trop géniale",
+            "content" => "blablablabla",
+            "id" => 2
+        ],
+        3 => [
+            "title" => "Balkany c'est trop génial",
+            "content" => "balblalblalb",
+            "id" => 3
+        ],
+        4 => [
+            "title" => "Balkany c'est pas trop génial",
+            "content" => "balblalblalb",
+            "id" => 4
+        ],
+        5 => [
+            "title" => "Je suis le titre 5",
+            "content" => "balblalblalb",
+            "id" => 5
+        ],
+        6 => [
+            "title" => "Je suis le titre 6",
+            "content" => "balblalblalb",
+            "id" => 6
+        ],
+        7 => [
+            "title" => "Je suis le titre 7",
+            "content" => "balblalblalb",
+            "id" => 7
+        ],
+        8 => [
+            "title" => "Je suis le titre 8",
+            "content" => "balblalblalb",
+            "id" => 8
+        ]
+    ];
 
-        return $this->render('article_show.html.twig', [
-            'article' => $articles[$id]
+
+    /**
+     * @Route("/articles", name="articleList")
+     */
+    public function articleList()
+    {
+        return $this->render('article_list.html.twig', [
+            'articles' => $this->articles
         ]);
+    }
 
-}
+    /**
+     * @Route("/article/{id}", name="articleShow")
+     */
+    public function articleShow($id)
+    {
+        // j'utilise la méthode render de l'AbstractController
+        // pour récupérer un fichier Twig, le transformer en HTML
+        // et le renvoyer en réponse HTTP au navigateur
+        // Pour utiliser des variables dans le fichier twig, je dois
+        // lui passer un tableau en second parametre, avec toutes les
+        // variables que je veux utiliser
+        return $this->render('article_show.html.twig', [
+            'article' => $this->articles[$id]
+        ]);
+    }
 
 }
