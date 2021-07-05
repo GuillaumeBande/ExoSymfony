79829f97a7bf79212de7a3c308fdd027082d193a
Guilllaume <bande.guillaume@gmail.com>
2021-07-05 11:50:11 +0200

    Les includes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 
 class ArticleController extends AbstractController
 {
@@ -13,42 +13,26 @@ class ArticleController extends AbstractController
         1 => [
             "title" => "La vaccination c'est trop géniale",
             "content" => "bablablblalba",
-            "id" => 1
+            "id" => 1,
+            "published" => true,
         ],
         2 => [
             "title" => "La vaccination c'est pas trop géniale",
             "content" => "blablablabla",
-            "id" => 2
+            "id" => 2,
+            "published" => true
         ],
         3 => [
             "title" => "Balkany c'est trop génial",
             "content" => "balblalblalb",
-            "id" => 3
+            "id" => 3,
+            "published" => false
         ],
         4 => [
             "title" => "Balkany c'est pas trop génial",
             "content" => "balblalblalb",
-            "id" => 4
-        ],
-        5 => [
-            "title" => "Je suis le titre 5",
-            "content" => "balblalblalb",
-            "id" => 5
-        ],
-        6 => [
-            "title" => "Je suis le titre 6",
-            "content" => "balblalblalb",
-            "id" => 6
-        ],
-        7 => [
-            "title" => "Je suis le titre 7",
-            "content" => "balblalblalb",
-            "id" => 7
-        ],
-        8 => [
-            "title" => "Je suis le titre 8",
-            "content" => "balblalblalb",
-            "id" => 8
+            "id" => 4,
+            "published" => true
         ]
     ];
 
@@ -56,7 +40,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/articles", name="articleList")
      */
-    public function articleList()
+    public function articleList(): Response
     {
         return $this->render('article_list.html.twig', [
             'articles' => $this->articles
@@ -64,9 +48,9 @@ class ArticleController extends AbstractController
     }
 
     /**
-     * @Route("/article/{id}", name="articleShow")
+     * @Route("/articles/{id}", name="articleShow")
      */
-    public function articleShow($id)
+    public function articleShow($id): Response
     {
         // j'utilise la méthode render de l'AbstractController
         // pour récupérer un fichier Twig, le transformer en HTML
